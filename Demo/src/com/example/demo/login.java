35b6c2973cae3c95b6446ed8630c1a9be2810c5c
nitiladp1y <gannaotu08033@126.com>
2021-06-08 12:10:56 +0800

    新建login  extends  MainActivity

---
+++ Demo/src/com/example/demo/login.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import android.os.Bundle;
+
+public class login extends MainActivity{
+	@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.login);
+}
+
+}
