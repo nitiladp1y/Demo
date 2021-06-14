f53ca6dbaace9c552ae7ff8a82d93860076263a1
nitiladp1y <gannaotu08033@126.com>
2021-06-14 11:48:20 +0800

    1.添加Button 2.跳转Intent

--- Demo/src/com/example/demo/MainActivity.java
+++ Demo/src/com/example/demo/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.demo;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button mybutton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		mybutton = (Button)findViewById(R.id.mybutton);
+		mybutton.setOnClickListener(new myButtonListener());
 	}
 
 	@Override
@@ -31,4 +36,17 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	class myButtonListener  implements OnClickListener{
+		
+		@Override
+	public void onClick(View v) {
+			Intent intent = new Intent();
+	
+			intent.setClass(MainActivity.this, login.class);
+			
+		// TODO Auto-generated method stub
+		
+	}
+		}
 }
